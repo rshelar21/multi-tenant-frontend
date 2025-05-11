6f5ddb2c0949019d5cf4cb2ee87c676ded7cd130
rshelar21 <rshelar796@gmail.com>
2025-05-12 00:58:38 +0530

    feat: shadcn & customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Input } from '@/components/ui/input';
+import { Progress } from '@/components/ui/progress';
+import { Textarea } from '@/components/ui/textarea';
+
 export default function Home() {
   return (
-    <div>
-      <h1>this is test commit</h1>
+    <div className="my-10 flex flex-col gap-10 px-10">
+      <Button variant="elevated">this is button</Button>
+      <Input placeholder="this is input" />
+      <Progress value={50} />
+      <Textarea placeholder="this is textarea" />
+      <Checkbox />
     </div>
   );
 }
